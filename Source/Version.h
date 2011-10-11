c834ae617ba8cbcc6be97d0ba295111ca7031be7
cvet <cvet@c2c6a35e-ad2b-4e1f-bb64-97b989be360b>
2011-10-11 08:50:03 +0000

    Next stable version.
    git-svn-id: https://cvet.by:8443/svn/FOnlineSource@282 c2c6a35e-ad2b-4e1f-bb64-97b989be360b
    
    Former-commit-id: 0bf88bc49fbfd1aee1f8c848573b9c9a92ac3d44

--- Source/Version.h
+++ Source/Version.h
@@ -1,11 +1,11 @@
 #ifndef ___VERSION___
 #define ___VERSION___
 
-#define SERVER_VERSION                  0x00000458
-#define CLIENT_VERSION                  0x00000359
-#define MAPPER_VERSION                  0x00000206
+#define SERVER_VERSION                  0x00000459
+#define CLIENT_VERSION                  0x00000360
+#define MAPPER_VERSION                  0x00000207
 
-#define MAPPER_VERSION_STR              "v.1.25.33"
+#define MAPPER_VERSION_STR              "v.1.25.34"
 #define DIALOG_EDITOR_VERSION_STR       "v.2.18.3"
 #define OBJECT_EDITOR_VERSION_STR       "v.2.14.3"
 
