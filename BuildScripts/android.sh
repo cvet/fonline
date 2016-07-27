e9533487b4b0fe8a6e700f229693e62e2c889c9e
cvet <cvet@c2c6a35e-ad2b-4e1f-bb64-97b989be360b>
2016-07-27 23:03:04 +0000

    Building automatization.
    git-svn-id: https://cvet.by:8443/svn/FOnlineSource/Main@1188 c2c6a35e-ad2b-4e1f-bb64-97b989be360b
    
    Former-commit-id: 8898a8957c7abac143e09cbf67547c2ee58de366

--- BuildScripts/android.sh
+++ BuildScripts/android.sh
@@ -14,8 +14,10 @@ export ANDROID_ABI=armeabi-v7a
 mkdir $ANDROID_ABI
 cd $ANDROID_ABI
 cmake -C $FO_SOURCE/BuildScripts/android.cache.cmake $FO_SOURCE/Source && make
+cd ../
 
 export ANDROID_ABI=arm64-v8a
 mkdir $ANDROID_ABI
 cd $ANDROID_ABI
 cmake -C $FO_SOURCE/BuildScripts/android.cache.cmake $FO_SOURCE/Source && make
+cd ../
