bb55444297a54c2d1d8108d8aa7cc16b7405fc74
cvet <cvet@c2c6a35e-ad2b-4e1f-bb64-97b989be360b>
2016-08-22 09:19:19 +0000

    All arch builds for windows.
    git-svn-id: https://cvet.by:8443/svn/FOnlineSource/Main@1207 c2c6a35e-ad2b-4e1f-bb64-97b989be360b
    
    Former-commit-id: 295cd2c460fde0413afadd450451b882b6cfb8e3

--- BuildScripts/windows.bat
+++ BuildScripts/windows.bat
@@ -16,7 +16,12 @@ PUSHD "%BUILD_DIR%\windows\x64"
 cmake -G "Visual Studio 14 2015 Win64" "%FO_SOURCE%\Source"
 POPD
 
-cmake --build "%BUILD_DIR%\windows\x86" --config RelWithDebInfo --target FOnline
+cmake --build "%BUILD_DIR%\windows\x64" --config RelWithDebInfo --target FOnlineServer
+cmake --build "%BUILD_DIR%\windows\x64" --config RelWithDebInfo --target Mapper
+cmake --build "%BUILD_DIR%\windows\x64" --config RelWithDebInfo --target ASCompiler
+cmake --build "%BUILD_DIR%\windows\x64" --config RelWithDebInfo --target FOnline
+
 cmake --build "%BUILD_DIR%\windows\x86" --config RelWithDebInfo --target FOnlineServer
 cmake --build "%BUILD_DIR%\windows\x86" --config RelWithDebInfo --target Mapper
 cmake --build "%BUILD_DIR%\windows\x86" --config RelWithDebInfo --target ASCompiler
+cmake --build "%BUILD_DIR%\windows\x86" --config RelWithDebInfo --target FOnline
