bbec95e0e00f4ab9796913f39f83354ed52c4ba2
cvet <cvet@c2c6a35e-ad2b-4e1f-bb64-97b989be360b>
2017-02-03 14:15:45 +0000

    iOS build script.
    git-svn-id: https://cvet.by:8443/svn/FOnlineSource/Main@1443 c2c6a35e-ad2b-4e1f-bb64-97b989be360b
    
    Former-commit-id: a35861513acaecf7c91e2bb82c49285fee46f89e

--- BuildScripts/ios.sh
+++ BuildScripts/ios.sh
@@ -1,28 +1,13 @@
 #!/bin/bash
 
 # Usage:
-# Install: freeglut3-dev, libssl-dev, libevent
-# export FO_SOURCE=<source> && $FO_SOURCE/BuildScripts/linux.sh
+# export FO_SOURCE=<source> && sudo -E $FO_SOURCE/BuildScripts/ios.sh
 
 if [ "$FO_CLEAR" = "TRUE" ]; then
-	rm -rf linux
+	rm -rf iOS
 fi
-mkdir linux
-cd linux
+mkdir ios
+cd ios
 
-mkdir x86
-cd x86
-#export CFLAGS=-m32
-cmake -G "Unix Makefiles" $FO_SOURCE/Source
-cd ../
-
-mkdir x64
-cd x64
-#export CFLAGS=-m64
-cmake -G "Unix Makefiles" $FO_SOURCE/Source
-cd ../
-
-cmake --build ./x86 --config RelWithDebInfo --target FOnline
-cmake --build ./x86 --config RelWithDebInfo --target FOnlineServer
-cmake --build ./x86 --config RelWithDebInfo --target Mapper
-cmake --build ./x86 --config RelWithDebInfo --target ASCompiler
+/Applications/CMake.app/Contents/bin/cmake -G Xcode -C "$FO_SOURCE/BuildScripts/ios.cache.cmake" $FO_SOURCE/Source
+/Applications/CMake.app/Contents/bin/cmake --build . --config RelWithDebInfo --target FOnline
