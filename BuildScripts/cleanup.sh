d5c924bec2aaa6700e373ad921897f8263114139
cvet <cvet@c2c6a35e-ad2b-4e1f-bb64-97b989be360b>
2017-02-07 08:44:18 +0000

    Clean up script fix.
    git-svn-id: https://cvet.by:8443/svn/FOnlineSource/Main@1468 c2c6a35e-ad2b-4e1f-bb64-97b989be360b
    
    Former-commit-id: 78a15d8cb495f5216c14eac1722046cce3a59f9d

--- BuildScripts/cleanup.sh
+++ BuildScripts/cleanup.sh
@@ -4,8 +4,8 @@ sudo apt-get -y update
 sudo apt-get -y install lftp
 
 if [ -n "$FO_FTP_DEST" ]; then
-	lftp -e "rm -r Client; exit" -u $FO_FTP_USER $FO_FTP_DEST
-	lftp -e "rm -r Server; exit" -u $FO_FTP_USER $FO_FTP_DEST
-	lftp -e "rm -r Mapper; exit" -u $FO_FTP_USER $FO_FTP_DEST
-	lftp -e "rm -r ASCompiler; exit" -u $FO_FTP_USER $FO_FTP_DEST
+	lftp -e "rm -rf Client; exit" -u $FO_FTP_USER $FO_FTP_DEST
+	lftp -e "rm -rf Server; exit" -u $FO_FTP_USER $FO_FTP_DEST
+	lftp -e "rm -rf Mapper; exit" -u $FO_FTP_USER $FO_FTP_DEST
+	lftp -e "rm -rf ASCompiler; exit" -u $FO_FTP_USER $FO_FTP_DEST
 fi
