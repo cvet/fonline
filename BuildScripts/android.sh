617be1595c491796dd72878921a5364f53346ac9
cvet <cvet@c2c6a35e-ad2b-4e1f-bb64-97b989be360b>
2017-02-07 07:51:49 +0000

    Build scripts fixes.
    git-svn-id: https://cvet.by:8443/svn/FOnlineSource/Main@1465 c2c6a35e-ad2b-4e1f-bb64-97b989be360b
    
    Former-commit-id: a7c8842cfc7f460bea22c168f90d2f559a460281

--- BuildScripts/android.sh
+++ BuildScripts/android.sh
@@ -52,7 +52,7 @@ cd ../
 
 if [ -n "$FO_FTP_DEST" ]; then
 	find Android/* | while read line; do
-		wput $line ftp://$FO_FTP_USER@$FO_FTP_DEST/Client/Android/
+		wput $line ftp://$FO_FTP_USER@$FO_FTP_DEST/Client/
 	done
 fi
 
