6e8b0ed8fef58173418a8da1adfe25deb8245ced
cvet <cvet@c2c6a35e-ad2b-4e1f-bb64-97b989be360b>
2017-02-07 08:41:26 +0000

    Build script fix.
    git-svn-id: https://cvet.by:8443/svn/FOnlineSource/Main@1467 c2c6a35e-ad2b-4e1f-bb64-97b989be360b
    
    Former-commit-id: aeea05da631d97d363a9aa966634a5466f2609eb

--- BuildScripts/ios.sh
+++ BuildScripts/ios.sh
@@ -10,7 +10,7 @@ mkdir iOS
 rm -rf iOS/*
 
 /Applications/CMake.app/Contents/bin/cmake -G Xcode -C "$SOURCE_FULL_PATH/BuildScripts/ios.cache.cmake" "$SOURCE_FULL_PATH/Source"
-/Applications/CMake.app/Contents/bin/cmake --build . --config RelWithDebInfo --target FOnline
+/Applications/CMake.app/Contents/bin/cmake --build . --config Release --target FOnline
 
 #xcodebuild -project "${PROJECT}/${TARGET}_${PLATFORM}/Build/xcode/Unity-iPhone.xcodeproj" -configuration Release -scheme "Unity-iPhone" clean archive -archivePath "${PROJECT}/${TARGET}_${PLATFORM}/Build/xcode/build/Unity-iPhone"
 #xcodebuild -exportArchive -archivePath "${PROJECT}/${TARGET}_${PLATFORM}/Build/xcode/build/Unity-iPhone.xcarchive" -exportFormat ipa -exportPath "${PROJECT}/${TARGET}_${PLATFORM}/Build/Unity-iPhone.ipa" -exportProvisioningProfile "Warfair"
