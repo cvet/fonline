e597e56ce0e64801bfbbb1311e5b488270f2f1ed
cvet <cvet@c2c6a35e-ad2b-4e1f-bb64-97b989be360b>
2017-02-07 13:44:11 +0000

    Raspberry Pi build scripts.
    git-svn-id: https://cvet.by:8443/svn/FOnlineSource/Main@1481 c2c6a35e-ad2b-4e1f-bb64-97b989be360b
    
    Former-commit-id: 641598dbbd3fd69bb721bb3bf96bd9d0f71d003e

---
+++ BuildScripts/raspberrypi.cache.cmake
@@ -0,0 +1,3 @@
+# CMake initial cache
+
+set( CMAKE_TOOLCHAIN_FILE "$ENV{SOURCE_FULL_PATH}/BuildScripts/raspberrypi.toolchain.cmake" CACHE PATH "" FORCE )
--- BuildScripts/raspberrypi.sh
+++ BuildScripts/raspberrypi.sh
@@ -1,2 +1,30 @@
- #!/bin/bash -e
- 
+#!/bin/bash -e
+
+export SOURCE_FULL_PATH=$(cd $FO_SOURCE; pwd)
+
+sudo apt-get -y update
+sudo apt-get -y install build-essential
+sudo apt-get -y install cmake
+sudo apt-get -y install wput
+sudo apt-get -y git
+
+mkdir -p $FO_BUILD_DEST
+cd $FO_BUILD_DEST
+mkdir -p raspberrypi
+cd raspberrypi
+mkdir -p RaspberryPi
+rm -rf RaspberryPi/*
+
+git clone https://github.com/raspberrypi/tools
+export PI_TOOLS_HOME=$PWD/tools
+
+cmake -G "Unix Makefiles" -C "$SOURCE_FULL_PATH/BuildScripts/raspberrypi.cache.cmake" "$SOURCE_FULL_PATH/Source"
+make -j4
+
+if [ -n "$FO_FTP_DEST" ]; then
+	wput RaspberryPi ftp://$FO_FTP_USER@$FO_FTP_DEST/Client/
+fi
+
+if [ -n "$FO_COPY_DEST" ]; then
+	cp -r RaspberryPi "$FO_COPY_DEST/Client"
+fi
