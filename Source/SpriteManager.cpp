016f105fb15d5e23388f67550c0c7c7c2a44e1d1
cvet <cvet@tut.by>
2018-11-13 19:43:44 +0300

    Hot fix

--- Source/SpriteManager.cpp
+++ Source/SpriteManager.cpp
@@ -255,10 +255,10 @@ bool SpriteManager::Init()
     glBindVertexArrayOES_ = (PFNGLBINDVERTEXARRAYOESPROC) dlsym( es_lib, "glBindVertexArrayOES" );
     glDeleteVertexArraysOES_ = (PFNGLDELETEVERTEXARRAYSOESPROC) dlsym( es_lib, "glDeleteVertexArraysOES" );
     glGenVertexArraysOES_ = (PFNGLGENVERTEXARRAYSOESPROC) dlsym( es_lib, "glGenVertexArraysOES" );
-    OGL_vertex_array_object = ( glBindVertexArrayOES && glDeleteVertexArraysOES && glGenVertexArraysOES );
+    OGL_vertex_array_object = ( glBindVertexArrayOES_ && glDeleteVertexArraysOES_ && glGenVertexArraysOES_ );
     glRenderbufferStorageMultisampleIMG_ = (PFNGLRENDERBUFFERSTORAGEMULTISAMPLEIMG) dlsym( es_lib, "glRenderbufferStorageMultisampleIMG" );
     glFramebufferTexture2DMultisampleIMG_ = (PFNGLFRAMEBUFFERTEXTURE2DMULTISAMPLEIMG) dlsym( es_lib, "glFramebufferTexture2DMultisampleIMG" );
-    OGL_framebuffer_multisample = OGL_texture_multisample = ( glRenderbufferStorageMultisampleIMG && glFramebufferTexture2DMultisampleIMG );
+    OGL_framebuffer_multisample = OGL_texture_multisample = ( glRenderbufferStorageMultisampleIMG_ && glFramebufferTexture2DMultisampleIMG_ );
     # endif
     # ifdef FO_IOS
     OGL_vertex_array_object = true;
