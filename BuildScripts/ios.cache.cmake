c95cb300376d62be209882c1b0af24a90f8281ae
cvet <cvet@tut.by>
2018-11-15 16:35:07 +0300

    Wip

--- BuildScripts/ios.cache.cmake
+++ BuildScripts/ios.cache.cmake
@@ -5,4 +5,3 @@ if( NOT DEFINED "ENV{SOURCE_FULL_PATH}" )
 endif()
 
 set( CMAKE_TOOLCHAIN_FILE "$ENV{SOURCE_FULL_PATH}/BuildScripts/ios.toolchain.cmake" CACHE PATH "" FORCE )
-add_definitions( "-D_LARGEFILE64_SOURCE" )
