03a7157fdddee15f38aea6b2873af91eac2cdaad
cvet <cvet@tut.by>
2018-11-15 22:22:01 +0300

    Wip

--- BuildScripts/ios.sh
+++ BuildScripts/ios.sh
@@ -15,7 +15,8 @@ rm -rf iOS/*
 pwd
 
 /Applications/CMake.app/Contents/bin/cmake -C "$SOURCE_FULL_PATH/BuildScripts/ios.cache.cmake" "$SOURCE_FULL_PATH/Source"
-/Applications/CMake.app/Contents/bin/make
+make
+make install
 
 #xcodebuild -project "${PROJECT}/${TARGET}_${PLATFORM}/Build/xcode/Unity-iPhone.xcodeproj" -configuration Release -scheme "Unity-iPhone" clean archive -archivePath "${PROJECT}/${TARGET}_${PLATFORM}/Build/xcode/build/Unity-iPhone"
 #xcodebuild -exportArchive -archivePath "${PROJECT}/${TARGET}_${PLATFORM}/Build/xcode/build/Unity-iPhone.xcarchive" -exportFormat ipa -exportPath "${PROJECT}/${TARGET}_${PLATFORM}/Build/Unity-iPhone.ipa" -exportProvisioningProfile "Warfair"
