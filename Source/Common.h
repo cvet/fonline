661be28de48c7896c8cbb3788b432dc2c34a3a1a
cvet <cvet@tut.by>
2018-11-21 23:10:52 +0300

    Fixed distributive generation with name contains white space

--- Source/Common.h
+++ Source/Common.h
@@ -2,7 +2,7 @@
 #define __COMMON__
 
 // Versions
-#define FONLINE_VERSION                           ( 833 )
+#define FONLINE_VERSION                           ( 834 )
 
 // Debugging
 // #define DEV_VERSION
