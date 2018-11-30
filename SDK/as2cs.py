e6994a655e5252ca72e0f0db81de5b30b3eadcd9
cvet <cvet@tut.by>
2018-11-30 20:56:48 +0300

    Build tuning

--- SDK/as2cs.py
+++ SDK/as2cs.py
@@ -1,5 +1,6 @@
 import fnmatch
 import os
+from tokenizer import tokenize, TOK
 
 inputDirs = ['Modules']
 
@@ -9,5 +10,15 @@ for inDir in inputDirs:
 		for filename in fnmatch.filter(filenames, '*.fos'):
 			asScripts.append(os.path.abspath(os.path.join(root, filename)))
 
-for m in asScripts:
-	print m
+for filePath in asScripts:
+	with open(filePath, 'rb') as f:
+		text = f.read()
+	
+	print filePath
+	for token in tokenize(text):
+		print "{0}: '{1}' {2}".format(
+				TOK.descr[token.kind],
+				token.txt or "-",
+				token.val or "")
+	
+	break
