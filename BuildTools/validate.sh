242b16d63c4b3761f5322d97ed07e2abb46024f4
cvet <cvet@tut.by>
2020-02-19 13:41:56 +0300

    Build fixes

--- BuildTools/validate.sh
+++ BuildTools/validate.sh
@@ -3,7 +3,10 @@
 CUR_DIR="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
 source $CUR_DIR/setup-env.sh
 
-$CUR_DIR/prepare-workspace.sh $1
+if [ "$1" != "mac" ] && [ "$1" != "ios" ]; then
+    $CUR_DIR/prepare-workspace.sh $1
+fi
+
 $CUR_DIR/build.sh $1 $2
 
 if [ "$2" = "unit-tests" ]; then
