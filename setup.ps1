bb888750010ad60bc1bacf4b252b9b2ad3cb525f
cvet <cvet@tut.by>
2020-02-21 22:49:16 +0300

    Project generation to setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -38,6 +38,17 @@ Function Test-BuildTools {
     }
 }
 
+Function Ask-Path-For-Project {
+    [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+
+    $folder = New-Object System.Windows.Forms.FolderBrowserDialog
+    $folder.Description = "Select a folder where project folder with files will be generated"
+
+    if ($folder.ShowDialog() -eq "OK") {
+        return $folder.SelectedPath
+    }
+}
+
 Write-Host "Hello, Developer! Let's see what's going on..."
 
 while ($True) {
@@ -117,8 +128,23 @@ while ($True) {
                 break
             }
         } elseif ($answer -Eq "gen") {
-            Write-Host "...Generate new project..."
-            # Todo:
+            $projectName = Read-Host -Prompt "Type project name"
+            if ($projectName) {
+                $projectPath = Ask-Path-For-Project
+                if ($projectPath) {
+                    if (!(Test-Path "$projectPath\$projectName")) {
+                        New-Item -Path $projectPath -Name $projectName -ItemType "directory" | Out-Null
+                    }
+                    $rootPath = Split-Path $MyInvocation.MyCommand.Path
+                    $rootPathEx = $rootPath.Replace("\", "\\")
+                    $projectPathEx = $projectPath.Replace("\", "\\")
+                    $rootPathWsl = wsl wslpath -a "$rootPathEx"
+                    $projectPathWsl = wsl wslpath -a "$projectPathEx"
+                    $rootPathWsl = $rootPathWsl.Trim().TrimEnd("/")
+                    $projectPathWsl = $projectPathWsl.Trim().TrimEnd("/")
+                    cmd /C "wsl cd '$projectPathWsl/$projectName'; '$rootPathWsl/BuildTools/generate-project.sh' '$projectName'"
+                }
+            }
         } elseif ($answer -Eq "") {
             Write-Host "Good luck!"
             return
