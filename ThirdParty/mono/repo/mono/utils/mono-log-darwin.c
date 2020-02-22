fb271f8ebbf0f530e0f5671d22cddccbf3dae89a
cvet <cvet@tut.by>
2020-02-22 19:41:26 +0300

    iOS compilation fix

--- ThirdParty/mono/repo/mono/utils/mono-log-darwin.c
+++ ThirdParty/mono/repo/mono/utils/mono-log-darwin.c
@@ -7,23 +7,8 @@
 
 #if defined(HOST_IOS)
 
-#include <asl.h>
+#include <os/log.h>
 #include "mono-logger-internals.h"
-static int
-to_asl_priority (GLogLevelFlags log_level)
-{
-	switch (log_level & G_LOG_LEVEL_MASK)
-	{
-		case G_LOG_LEVEL_ERROR:     return ASL_LEVEL_CRIT;
-		case G_LOG_LEVEL_CRITICAL:  return ASL_LEVEL_ERR;
-		case G_LOG_LEVEL_WARNING:   return ASL_LEVEL_WARNING;
-		case G_LOG_LEVEL_MESSAGE:   return ASL_LEVEL_NOTICE;
-		case G_LOG_LEVEL_INFO:      return ASL_LEVEL_INFO;
-		case G_LOG_LEVEL_DEBUG:     return ASL_LEVEL_DEBUG;
-	}
-	return ASL_LEVEL_ERR;
-}
-
 void
 mono_log_open_asl (const char *path, void *userData)
 {
@@ -32,10 +17,40 @@ mono_log_open_asl (const char *path, void *userData)
 void
 mono_log_write_asl (const char *log_domain, GLogLevelFlags level, mono_bool hdr, const char *message)
 {
-	asl_log (NULL, NULL, to_asl_priority (level), "%s%s%s\n",
-		log_domain != NULL ? log_domain : "",
-		log_domain != NULL ? ": " : "",
-		message);
+	switch (level & G_LOG_LEVEL_MASK)
+	{
+		case G_LOG_LEVEL_MESSAGE:
+			os_log (OS_LOG_DEFAULT, "%s%s%s\n",
+				log_domain != NULL ? log_domain : "",
+				log_domain != NULL ? ": " : "",
+				message);
+			break;
+		case G_LOG_LEVEL_INFO:
+			os_log_info (OS_LOG_DEFAULT, "%s%s%s\n",
+				log_domain != NULL ? log_domain : "",
+				log_domain != NULL ? ": " : "",
+				message);
+			break;
+		case G_LOG_LEVEL_DEBUG:
+			os_log_debug (OS_LOG_DEFAULT, "%s%s%s\n",
+				log_domain != NULL ? log_domain : "",
+				log_domain != NULL ? ": " : "",
+				message);
+			break;
+		case G_LOG_LEVEL_ERROR:
+		case G_LOG_LEVEL_WARNING:
+			os_log_error (OS_LOG_DEFAULT, "%s%s%s\n",
+				log_domain != NULL ? log_domain : "",
+				log_domain != NULL ? ": " : "",
+				message);
+		case G_LOG_LEVEL_CRITICAL:
+		default:
+			os_log_fault (OS_LOG_DEFAULT, "%s%s%s\n",
+				log_domain != NULL ? log_domain : "",
+				log_domain != NULL ? ": " : "",
+				message);
+			break;
+	}
 
 	if (level & G_LOG_LEVEL_ERROR)
 		abort();
