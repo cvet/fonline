75546bcd37ac64e0d935be62c23ea509ef92b8d5
cvet <cvet@tut.by>
2020-10-25 17:32:03 +0300

    Fixes for gcc and clang-cl builds

--- BuildTools/validate.bat
+++ BuildTools/validate.bat
@@ -29,9 +29,17 @@ if [%1] == [win32-client] (
     set BUILD_TARGET=FONLINE_BUILD_CLIENT
     set BUILD_ARCH=x64
     set BUILD_CACHE=uwp.cache.cmake
+) else if [%1] == [win-clang-client] (
+    set BUILD_TARGET=FONLINE_BUILD_CLIENT
+    set BUILD_ARCH=x64
+    set BUILD_TOOLSET=LLVM_v142
 ) else if [%1] == [win64-server] (
     set BUILD_TARGET=FONLINE_BUILD_SERVER
     set BUILD_ARCH=x64
+) else if [%1] == [win-clang-server] (
+    set BUILD_TARGET=FONLINE_BUILD_SERVER
+    set BUILD_ARCH=x64
+    set BUILD_TOOLSET=LLVM_v142
 ) else if [%1] == [win32-single] (
     set BUILD_TARGET=FONLINE_BUILD_SINGLE
     set BUILD_ARCH=Win32
@@ -42,15 +50,27 @@ if [%1] == [win32-client] (
     set BUILD_TARGET=FONLINE_BUILD_SINGLE
     set BUILD_ARCH=x64
     set BUILD_CACHE=uwp.cache.cmake
+) else if [%1] == [win64-clang-single] (
+    set BUILD_TARGET=FONLINE_BUILD_SINGLE
+    set BUILD_ARCH=x64
+    set BUILD_TOOLSET=LLVM_v142
 ) else if [%1] == [win64-mapper] (
     set BUILD_TARGET=FONLINE_BUILD_MAPPER
     set BUILD_ARCH=x64
 ) else if [%1] == [win64-ascompiler] (
     set BUILD_TARGET=FONLINE_BUILD_ASCOMPILER
     set BUILD_ARCH=x64
+) else if [%1] == [win-clang-ascompiler] (
+    set BUILD_TARGET=FONLINE_BUILD_ASCOMPILER
+    set BUILD_ARCH=x64
+    set BUILD_TOOLSET=LLVM_v142
 ) else if [%1] == [win64-baker] (
     set BUILD_TARGET=FONLINE_BUILD_BAKER
     set BUILD_ARCH=x64
+) else if [%1] == [win-clang-baker] (
+    set BUILD_TARGET=FONLINE_BUILD_BAKER
+    set BUILD_ARCH=x64
+    set BUILD_TOOLSET=LLVM_v142
 ) else (
     echo Invalid build argument, allowed only win32 or win64
     exit /b 1
@@ -60,7 +80,9 @@ set BUILD_DIR=validate-%1
 if not exist %BUILD_DIR% mkdir %BUILD_DIR%
 cd %BUILD_DIR%
 
-if [%BUILD_CACHE%] == [] (
+if [%BUILD_TOOLSET%] != [] (
+    cmake -A %BUILD_ARCH% -T %BUILD_TOOLSET% -DCMAKE_BUILD_TYPE=Debug -D%BUILD_TARGET%=1 -DFONLINE_UNIT_TESTS=0 "%FO_ROOT%"
+) else if [%BUILD_CACHE%] == [] (
     cmake -A %BUILD_ARCH% -DCMAKE_BUILD_TYPE=Debug -D%BUILD_TARGET%=1 -DFONLINE_UNIT_TESTS=0 "%FO_ROOT%"
 ) else (
     cmake -A %BUILD_ARCH% -C "%FO_ROOT%\BuildTool\%BUILD_CACHE%" -DCMAKE_BUILD_TYPE=Debug -D%BUILD_TARGET%=1 -DFONLINE_UNIT_TESTS=0 "%FO_ROOT%"
--- BuildTools/validate.sh
+++ BuildTools/validate.sh
@@ -7,8 +7,9 @@ if [ "$1" = "linux-client" ]; then
     TARGET=linux
     BUILD_TARGET="-DFONLINE_BUILD_CLIENT=1 -DFONLINE_UNIT_TESTS=0 -DCMAKE_BUILD_TYPE=Debug"
 elif [ "$1" = "linux-gcc-client" ]; then
-    TARGET=linux-gcc
+    TARGET=linux
     BUILD_TARGET="-DFONLINE_BUILD_CLIENT=1 -DFONLINE_UNIT_TESTS=0 -DCMAKE_BUILD_TYPE=Debug"
+	USE_GCC=1
 elif [ "$1" = "android-arm-client" ]; then
     TARGET=android
     BUILD_TARGET="-DFONLINE_BUILD_CLIENT=1 -DFONLINE_UNIT_TESTS=0 -DCMAKE_BUILD_TYPE=Debug"
