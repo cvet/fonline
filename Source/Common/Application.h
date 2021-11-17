1d9b80b59985e0c32ff5fd1c1316e42ce0cf01b0
cvet <cvet@tut.by>
2021-11-17 12:56:51 +0300

    Backward-cpp setup + allowing obtain stacktrace on windows

--- Source/Common/Application.h
+++ Source/Common/Application.h
@@ -480,7 +480,7 @@ class MessageBox final
 public:
     MessageBox() = delete;
 
-    static void ShowErrorMessage(string_view message, string_view traceback);
+    static void ShowErrorMessage(string_view title, string_view message, string_view traceback);
 };
 
 extern void InitApplication(GlobalSettings& settings);
