2900e027fc47e677e93e450ae8c2ced416521ce0
cvet <cvet@tut.by>
2022-05-02 09:46:27 +0300

    AS hstring fix

--- Source/Scripting/AngelScriptScripting-Template.cpp
+++ Source/Scripting/AngelScriptScripting-Template.cpp
@@ -1174,7 +1174,7 @@ void SCRIPTING_CLASS::InitAngelScriptScripting(INIT_ARGS)
     }
 
     // Register hstring
-    AS_VERIFY(engine->RegisterObjectType("hstring", sizeof(hstring), asOBJ_VALUE | asOBJ_POD | asGetTypeTraits<hstring>()));
+    AS_VERIFY(engine->RegisterObjectType("hstring", sizeof(hstring), asOBJ_VALUE | asOBJ_POD | asOBJ_APP_CLASS_ALLINTS | asGetTypeTraits<hstring>()));
     AS_VERIFY(engine->RegisterObjectBehaviour("hstring", asBEHAVE_CONSTRUCT, "void f()", SCRIPT_FUNC_THIS(HashedString_Construct), SCRIPT_FUNC_THIS_CONV));
     AS_VERIFY(engine->RegisterObjectBehaviour("hstring", asBEHAVE_CONSTRUCT, "void f(const hstring &in)", SCRIPT_FUNC_THIS(HashedString_ConstructCopy), SCRIPT_FUNC_THIS_CONV));
     AS_VERIFY(engine->RegisterObjectBehaviour("hstring", asBEHAVE_CONSTRUCT, "void f(const string &in)", asFUNCTION(HashedString_ConstructFromString), asCALL_GENERIC, game_engine));
