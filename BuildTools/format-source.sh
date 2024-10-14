a8b9742e62619759eb1bbaa794a373a2d605a5f4
cvet <cvet@tut.by>
2024-10-14 09:12:38 +0000

    Format script fix

--- BuildTools/format-source.sh
+++ BuildTools/format-source.sh
@@ -2,7 +2,7 @@
 
 CUR_DIR="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
 
-cd "%CUR_DIR/../Source" && clang-format -i \
+cd "$CUR_DIR/../Source" && clang-format -i \
 Applications/*.cpp \
 Client/*.cpp Client/*.h \
 Common/*.cpp Common/*.h \
