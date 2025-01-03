fc2156285da7c7b931e914756e9ea7ba78990439
cvet <cvet@tut.by>
2025-01-03 17:14:05 +0300

    Copyright to year 2025

--- Source/Tools/ImageBaker.cpp
+++ Source/Tools/ImageBaker.cpp
@@ -10,7 +10,7 @@
 //
 // MIT License
 //
-// Copyright (c) 2006 - 2024, Anton Tsvetinskiy aka cvet <cvet@tut.by>
+// Copyright (c) 2006 - 2025, Anton Tsvetinskiy aka cvet <cvet@tut.by>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
