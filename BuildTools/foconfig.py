259c2d016c19724c7c1ffcd76d92d2882309d95d
cvet <cvet@tut.by>
2025-05-10 20:52:15 +0300

    Scripts formatting

--- BuildTools/foconfig.py
+++ BuildTools/foconfig.py
@@ -12,15 +12,20 @@ class ConfigSection:
     def getBool(self, key, default=None):
         self.checkKey(key, default)
         value = self.content.get(key, str(default))
-        return True if value.lower() == 'true' else False if value.lower() == 'false' else int(value) != 0
+        if value.lower() == 'true':
+            return True
+        elif value.lower() == 'false':
+            return False
+        return int(value) != 0
 
     def getInt(self, key, default=None):
         self.checkKey(key, default)
         return int(self.content.get(key, str(default)))
-    
+
     def checkKey(self, key, default):
         if default is None and key not in self.content:
             raise KeyError(f"Key '{key}' not found in section '{self.name}'")
+
 
 class ConfigParser:
     def __init__(self):
@@ -29,7 +34,6 @@ class ConfigParser:
     def loadFromFile(self, path):
         with open(path, 'r', encoding='utf-8-sig') as f:
             lines = f.readlines()
-
         self.loadFromLines(lines)
 
     def loadFromLines(self, lines):
@@ -37,27 +41,30 @@ class ConfigParser:
         sectionContent = {}
 
         for line in lines:
-            comm = line.find('#')
-            if comm != -1:
-                line = line[:comm]
+            # Remove comments
+            commentIndex = line.find('#')
+            if commentIndex != -1:
+                line = line[:commentIndex]
 
             line = line.strip()
 
+            # Handle section headers
             if line.startswith('[') and line.endswith(']'):
                 self.sections.append(ConfigSection(sectionName, sectionContent))
                 sectionName = line[1:-1].strip()
                 sectionContent = {}
 
+            # Handle key-value pairs
             elif '=' in line:
                 key, value = line.split('=', 1)
                 key = key.strip()
                 value = value.strip()
-
                 if key:
                     sectionContent[key] = value
-        
+
+        # Add the last section
         self.sections.append(ConfigSection(sectionName, sectionContent))
-    
+
     def mainSection(self):
         return self.sections[0]
 
--- BuildTools/starter.py
+++ BuildTools/starter.py
@@ -11,12 +11,13 @@ import foconfig
 BIN_TYPES = ['Server', 'Client', 'Mapper', 'Editor', 'Baker', 'ASCompiler', 'Tests']
 BIN_NEED_BACKED_RESOURCES = ['Server', 'Client', 'Mapper']
 
+# Argument parser setup
 parser = argparse.ArgumentParser(description='FOnline packager')
 parser.add_argument('-maincfg', dest='maincfg', required=True, help='Main config path')
 parser.add_argument('-devname', dest='devname', required=True, help='Dev game name')
-parser.add_argument('-buildhash', dest='buildhash', required=True, help='build hash')
-parser.add_argument('-bininput', dest='bininput', required=True, action='append', default=[], help='binary input dir')
-parser.add_argument('-config', dest='config', action='append', default=[], help='config option')
+parser.add_argument('-buildhash', dest='buildhash', required=True, help='Build hash')
+parser.add_argument('-bininput', dest='bininput', required=True, action='append', default=[], help='Binary input directory')
+parser.add_argument('-config', dest='config', action='append', default=[], help='Config option')
 args = parser.parse_args()
 
 assert platform.system() in ['Linux', 'Darwin', 'Windows'], 'Invalid OS'
@@ -25,146 +26,137 @@ fomain = foconfig.ConfigParser()
 fomain.loadFromFile(args.maincfg)
 
 def log(*text):
-	print('[Starter]', *text, flush=True)
+    print('[Starter]', *text, flush=True)
 
 try:
-	import easygui
-	hasEasyGui = True
-except:
-	hasEasyGui = False
-	log('Please install easygui for better experience')
-	log('Type in console: pip install easygui')
+    import easygui
+    hasEasyGui = True
+except ImportError:
+    hasEasyGui = False
+    log('Please install easygui for better experience')
+    log('Type in console: pip install easygui')
 
 def error(message):
-	log(message)
-	if hasEasyGui:
-		easygui.msgbox(message, args.devname + ' Starter')
-	else:
-		input('Press enter to continue')
-	sys.exit(1)
+    log(message)
+    if hasEasyGui:
+        easygui.msgbox(message, args.devname + ' Starter')
+    else:
+        input('Press enter to continue')
+    sys.exit(1)
 
 def choicebox(message, choices):
-	log(message)
-	if hasEasyGui:
-		return easygui.choicebox(message, args.devname + ' Starter', choices=choices)
-	else:
-		index = 1
-		for choice in choices:
-			log(f'{index}) {choice}' + (' (default)' if index == 1 else ''))
-			index += 1
-		try:
-			index = int(input(': ') or 1)
-		except:
-			index = 0
-		return choices[index - 1] if index >= 1 and index <= len(choices) else None
+    log(message)
+    if hasEasyGui:
+        return easygui.choicebox(message, args.devname + ' Starter', choices=choices)
+    else:
+        for index, choice in enumerate(choices, start=1):
+            log(f'{index}) {choice}' + (' (default)' if index == 1 else ''))
+        try:
+            index = int(input(': ') or 1)
+        except ValueError:
+            index = 0
+        return choices[index - 1] if 1 <= index <= len(choices) else None
 
-def checkDirHash(dir, inputType):
-	buildHashPath = os.path.join(dir, inputType + '.build-hash')
-	if os.path.isfile(buildHashPath):
-		with open(buildHashPath, 'r', encoding='utf-8-sig') as f:
-			return f.read() == args.buildhash
-	return False
+def checkDirHash(directory, inputType):
+    buildHashPath = os.path.join(directory, inputType + '.build-hash')
+    if os.path.isfile(buildHashPath):
+        with open(buildHashPath, 'r', encoding='utf-8-sig') as f:
+            return f.read() == args.buildhash
+    return False
 
 try:
-	log('Build hash', args.buildhash)
-	bakeOutput = fomain.mainSection().getStr('BakeOutput')
-	bakingEntry = os.path.realpath(bakeOutput)
+    log('Build hash', args.buildhash)
+    bakeOutput = fomain.mainSection().getStr('BakeOutput')
+    bakingEntry = os.path.realpath(bakeOutput)
 
-	if not checkDirHash(bakingEntry, 'Resources'):
-		log('Baked resources not found')
-		bakingEntry = None
+    if not checkDirHash(bakingEntry, 'Resources'):
+        log('Baked resources not found')
+        bakingEntry = None
 
-	binaryEntries = []
-	
-	for binInput in args.bininput:
-		absInput = os.path.realpath(binInput)
-		log('Scan for binaries', absInput)
-		
-		for entry in os.scandir(absInput):
-			binDir = os.path.join(absInput, entry.name)
-			def verifyBinaryHash():
-				return checkDirHash(binDir, args.devname + '_' + entry.name.split('-')[0])
-			if platform.system() == 'Windows' and '-Windows-' in entry.name and verifyBinaryHash():
-				binaryEntries.append(binDir)
-			elif platform.system() == 'Linux' and '-Linux-' in entry.name and verifyBinaryHash():
-				binaryEntries.append(binDir)
-			elif platform.system() == 'Darwin' and '-macOS-' in entry.name and verifyBinaryHash():
-				binaryEntries.append(binDir)
+    binaryEntries = []
+    for binInput in args.bininput:
+        absInput = os.path.realpath(binInput)
+        log('Scan for binaries', absInput)
+        for entry in os.scandir(absInput):
+            binDir = os.path.join(absInput, entry.name)
+            def verifyBinaryHash():
+                return checkDirHash(binDir, args.devname + '_' + entry.name.split('-')[0])
+            if platform.system() == 'Windows' and '-Windows-' in entry.name and verifyBinaryHash():
+                binaryEntries.append(binDir)
+            elif platform.system() == 'Linux' and '-Linux-' in entry.name and verifyBinaryHash():
+                binaryEntries.append(binDir)
+            elif platform.system() == 'Darwin' and '-macOS-' in entry.name and verifyBinaryHash():
+                binaryEntries.append(binDir)
 
-	if not bakingEntry:
-		binaryEntries = list(filter(lambda x: os.path.basename(x).split('-')[0] not in BIN_NEED_BACKED_RESOURCES, binaryEntries))
-	binaryEntries = list(filter(lambda x: len(os.path.basename(x).split('-')) >= 3, binaryEntries))
-	binaryEntries = list(filter(lambda x: os.path.basename(x).split('-')[0] in BIN_TYPES, binaryEntries))
-	binaryEntries.sort(key=lambda x: (BIN_TYPES.index(os.path.basename(x).split('-')[0]), os.path.basename(x).split('-')[3:]))
+    if not bakingEntry:
+        binaryEntries = list(filter(lambda x: os.path.basename(x).split('-')[0] not in BIN_NEED_BACKED_RESOURCES, binaryEntries))
+    binaryEntries = list(filter(lambda x: len(os.path.basename(x).split('-')) >= 3, binaryEntries))
+    binaryEntries = list(filter(lambda x: os.path.basename(x).split('-')[0] in BIN_TYPES, binaryEntries))
+    binaryEntries.sort(key=lambda x: (BIN_TYPES.index(os.path.basename(x).split('-')[0]), os.path.basename(x).split('-')[3:]))
 
-	if not binaryEntries:
-		error('Binaries not found or outdated')
+    if not binaryEntries:
+        error('Binaries not found or outdated')
 
-	for binaryEntry in binaryEntries:
-		log('Found binary entry', os.path.relpath(binaryEntry))
+    for binaryEntry in binaryEntries:
+        log('Found binary entry', os.path.relpath(binaryEntry))
 
-	configs = [subConfig.getStr('Name') for subConfig in fomain.getSections('SubConfig')]
-	choices = []
-	choiceActions = []
+    configs = [subConfig.getStr('Name') for subConfig in fomain.getSections('SubConfig')]
+    choices = []
+    choiceActions = []
 
-	for binEntry in binaryEntries:
-		nameParts = os.path.basename(binEntry).split('-')
-		binType = nameParts[0]
-		archType = nameParts[2]
-		buildType = nameParts[3] if len(nameParts) > 3 else ''
-		
-		def action(binEntry=binEntry, binType=binType, buildType=buildType):
-			config = None
-			
-			if binType in ['Server', 'Client', 'Mapper', 'Editor']:
-				binConfigs = configs[:]
-				if binType in binConfigs:
-					binConfigs.remove(binType)
-					binConfigs.insert(0, binType)
-				config = choicebox('Select config to start', binConfigs)
-				if not config:
-					sys.exit(0)
-			elif binType == 'Baker':
-				bakingType = choicebox('Select baking mode', ['Iterative', 'Force'])
-				if not bakingType:
-					sys.exit(0)
+    for binEntry in binaryEntries:
+        nameParts = os.path.basename(binEntry).split('-')
+        binType = nameParts[0]
+        archType = nameParts[2]
+        buildType = nameParts[3] if len(nameParts) > 3 else ''
 
-			exePath = os.path.join(binEntry, args.devname + '_' + binType)
-			if platform.system() == 'Windows':
-				exePath += '.exe'
-			exePath = os.path.relpath(exePath)
-			
-			exeArgs = []
-			exeArgs += ['-ApplyConfig', os.path.relpath(args.maincfg)]
-			if config != None:
-				exeArgs += ['-ApplySubConfig', config]
-			if binType == 'Baker':
-				exeArgs += ['-ForceBaking', 'True' if bakingType == 'Force' else 'False']
-			for entry in args.config:
-				exeArgs += ['-' + entry.split(',')[0], entry.split(',')[1]]
-			
-			needWait = binType in ['Baker', 'ASCompiler'] or 'San_' in buildType
-			
-			if not needWait and (platform.system() == 'Linux' or platform.system() == 'Darwin'):
-				exeArgs += ['--fork']
-			
-			log('Run and wait' if needWait else 'Run', exePath)
-			
-			proc = subprocess.Popen([exePath] + exeArgs)
-			
-			if needWait:
-				proc.wait()
-				if proc.returncode:
-					error('Something went wrong\nVerify log messages')
-				else:
-					input('Press enter to continue')
-		
-		choices.append(binType + (' ' + buildType if buildType else '') + (' ' + archType if archType not in ['win64', 'x64'] else ''))
-		choiceActions.append(action)
+        def action(binEntry=binEntry, binType=binType, buildType=buildType):
+            config = None
+            if binType in ['Server', 'Client', 'Mapper', 'Editor']:
+                binConfigs = configs[:]
+                if binType in binConfigs:
+                    binConfigs.remove(binType)
+                    binConfigs.insert(0, binType)
+                config = choicebox('Select config to start', binConfigs)
+                if not config:
+                    sys.exit(0)
+            elif binType == 'Baker':
+                bakingType = choicebox('Select baking mode', ['Iterative', 'Force'])
+                if not bakingType:
+                    sys.exit(0)
 
-	reply = choicebox('Select binary to start', choices)
-	if reply:
-		choiceActions[choices.index(reply)]()
+            exePath = os.path.join(binEntry, args.devname + '_' + binType)
+            if platform.system() == 'Windows':
+                exePath += '.exe'
+            exePath = os.path.relpath(exePath)
+
+            exeArgs = ['-ApplyConfig', os.path.relpath(args.maincfg)]
+            if config is not None:
+                exeArgs += ['-ApplySubConfig', config]
+            if binType == 'Baker':
+                exeArgs += ['-ForceBaking', 'True' if bakingType == 'Force' else 'False']
+            for entry in args.config:
+                exeArgs += ['-' + entry.split(',')[0], entry.split(',')[1]]
+
+            needWait = binType in ['Baker', 'ASCompiler'] or 'San_' in buildType
+            if not needWait and platform.system() in ['Linux', 'Darwin']:
+                exeArgs += ['--fork']
+
+            log('Run and wait' if needWait else 'Run', exePath)
+            proc = subprocess.Popen([exePath] + exeArgs)
+            if needWait:
+                proc.wait()
+                if proc.returncode:
+                    error('Something went wrong\nVerify log messages')
+                else:
+                    input('Press enter to continue')
+
+        choices.append(binType + (' ' + buildType if buildType else '') + (' ' + archType if archType not in ['win64', 'x64'] else ''))
+        choiceActions.append(action)
+
+    reply = choicebox('Select binary to start', choices)
+    if reply:
+        choiceActions[choices.index(reply)]()
 
 except Exception as ex:
-	error(f'Unhandled exception: {ex}\n{traceback.format_exc()}')
+    error(f'Unhandled exception: {ex}\n{traceback.format_exc()}')
